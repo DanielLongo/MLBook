a8304c241f2eb6041c27e5dce4e180f786fd70e9
Daniel Longo <daniellongo@Daniels-MacBook.local>
2020-01-22 15:14:32 -0800

    first

---
+++ data.py
@@ -0,0 +1,81 @@
+import re
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+
+
+def create_text_chunks(text, chunk_length):
+    # creates text chunks also with next word
+    assert (type(text) == str), "Text input need to be a string "
+    # print("type of text", type(text))
+    split = text.split(" ")
+    out_X = []
+    out_y = []
+    for i in range(0, len(split) - chunk_length, chunk_length):
+        start = i
+        end = i + chunk_length
+        cur_chunk = split[start: end]
+        out_X.append(cur_chunk)
+        out_y.append(split[end])
+
+    assert (len(out_X) == len(out_y)), "len of X and y should correspond"
+    print('Len of chunks', len(out_X))
+    return out_X, out_y
+
+
+def get_tokenizer(text):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(text)
+    return tokenizer
+
+
+def load_document(filename):
+    with open(filename, "r") as f:
+        text = f.read()
+    cleaned_text = clean_text(text)
+    print(type(cleaned_text))
+    print("num unique words", )
+    out_X, out_y = create_text_chunks(cleaned_text, 50)
+    tokenizer = get_tokenizer(cleaned_text.split(" "))
+    out_X = tokenizer.texts_to_sequences(out_X)
+    out_y = tokenizer.texts_to_sequences(out_y)
+    print("num words", len(tokenizer.word_index) + 1)
+    print(len(tokenizer.word_index) + 1)
+    # out_X = [tokenizer.texts_to_sequences(chunk) for chunk in out_X]
+    # out_y = [tokenizer.texts_to_sequences(chunk) for chunk in out_y]
+    print("sample X", out_X[0])
+    print("sample y", out_y[0])
+    out_X = np.asarray(out_X)
+    out_y = np.asarray(out_y)
+    return out_X, out_y
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(' +', ' ', text)
+    desired_chars = "abcdefghijklmnopqrstuvwxyz "
+    cleaned = ""
+    for char in text:
+        if char in desired_chars:
+            cleaned += char
+        else:
+            pass
+            # print("char not desired", char)
+
+    cleaned = cleaned.strip("")
+    cleaned = cleaned.split(" ")
+    out = []
+    undesired_words = [""]
+    for word in cleaned:
+        if word in undesired_words:
+            continue
+        out.append(word)
+    cleaned = " ".join(out)
+    return cleaned
+
+
+if __name__ == "__main__":
+    filename = "Gutenberg/txt/Ralph Waldo Emerson___Essays.txt"
+    out_X, out_y = load_document(filename)
+    print("out_X", out_X.shape)
+    print("out_y", out_y.shape)
+    print("Finished data.py")
---
+++ model.py
@@ -0,0 +1,47 @@
+from pickle import dump
+
+import keras
+from keras import Sequential
+from keras.engine.saving import load_model
+from keras.layers import Embedding, LSTM, Dense
+import numpy as np
+
+from data import load_document
+
+def indices_to_one_hot(data, nb_classes):
+    """Convert an iterable of indices to one-hot encoded labels."""
+    targets = np.array(data).reshape(-1)
+    return np.eye(nb_classes)[targets]
+
+def main():
+    seq_length = 50
+    vocab_size = 16739
+    model = Sequential()
+    model.add(Embedding(vocab_size, 50, input_length=seq_length))
+    model.add(LSTM(100, return_sequences=True))
+    model.add(LSTM(100))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(vocab_size, activation='softmax'))
+    print(model.summary())
+
+    # compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit model
+    filename = "Gutenberg/txt/Ralph Waldo Emerson___Essays.txt"
+    X, y = load_document(filename)
+    # X = X.T
+    y = indices_to_one_hot(y, vocab_size)
+    print("X shape", X.shape)
+    print("y shape", y.shape)
+    model.fit(X, y, batch_size=128, epochs=100)
+    # save the model to file
+    model.save('model.h5')
+    # dump(tokenizer, open('tokenizer.pkl', 'wb'))
+
+def predict():
+    model = load_model('model.h5')
+
+
+
+if __name__ == "__main__":
+    main()
