bc8222ce5d0c3925abbde324e520255b7a2794ce
DanielLongo <danieldominiclongo@gmail.com>
2020-01-22 21:02:42 -0800

    updates

--- model.py
+++ model.py
@@ -5,8 +5,13 @@ from keras import Sequential
 from keras.engine.saving import load_model
 from keras.layers import Embedding, LSTM, Dense
 import numpy as np
+import tensorflow as tf
 
 from data import load_document
+
+tf.debugging.set_log_device_placement(True)
+
+# for word, index in tokenizer.word_index.items():
 
 def indices_to_one_hot(data, nb_classes):
     """Convert an iterable of indices to one-hot encoded labels."""
@@ -14,6 +19,7 @@ def indices_to_one_hot(data, nb_classes):
     return np.eye(nb_classes)[targets]
 
 def main():
+    epochs = 300
     seq_length = 50
     vocab_size = 16739
     model = Sequential()
@@ -33,7 +39,7 @@ def main():
     y = indices_to_one_hot(y, vocab_size)
     print("X shape", X.shape)
     print("y shape", y.shape)
-    model.fit(X, y, batch_size=128, epochs=100)
+    model.fit(X, y, batch_size=128, epochs=300)
     # save the model to file
     model.save('model.h5')
     # dump(tokenizer, open('tokenizer.pkl', 'wb'))
